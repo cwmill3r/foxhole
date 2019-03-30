9b0da838388450317c0b8f8a50245c8e28034228
Chris Miller <cwmiller@asu.edu>
2019-03-30 12:27:26 -0700

    added services to get a survey and to get unresponded surveys

--- foxhole/AccountServices.asmx.cs
+++ foxhole/AccountServices.asmx.cs
@@ -256,8 +256,147 @@ namespace foxhole
             {
                 return surveyID;
             }
+            sqlConnection.Close(); // I am just closing and opening again for simplicity - optimize later
             return -1;
         }
+
+        // This is a service we can use in a loop on the JavaScript side to fill in the recipients
+        // when we create a survey. When finished we will take the functionality out of the above method
+        // and use these two together... altogether avoiding passing an array to any services making testing easier
+        // The only thing that could screw this up is if the loop is too fast to actually run the service... :/
+        [WebMethod(EnableSession = true)]
+        public bool FillRecipientsTable(int recipient_eID, int surveyID) // we get surveyID back from the above method
+        {
+            bool success = false;
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = $"insert into response (answer, date, eID, sID, completed) " +
+                $"values(null, null, {recipient_eID}, {surveyID}, 0); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                surveyID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+                return success = true;
+            }
+            catch (Exception e)
+            {
+            }
+
+            sqlConnection.Close(); // I am just closing and opening again for simplicity - optimize later
+            return success;
+        }
+
+        // Service to get all surveys that a person has been sent but hasnt responded to
+        [WebMethod(EnableSession = true)]
+        public List<Survey> GetUnrespondedSurveys(int eID) // we get surveyID aka eID back from the above method
+        {
+            List<Survey> tmpSurveys = new List<Survey>(); // kind of a temporary list (we return this)
+            
+            //WE ONLY LET LOGGED IN USERS SEE THEIR SURVEYS!
+            if (Session["eID"] != null)
+            {
+                DataTable sqlDt = new DataTable("surveys");
+
+                string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+                string sqlSelect = $"select * from survey, response where survey.sID = response.sID and response.eID = {eID} and response.completed = 0";
+
+                MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+                MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+                //gonna use this to fill a data table
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+                //filling the data table
+                sqlDa.Fill(sqlDt);
+
+                //loop through each row in the dataset, creating instances
+                //of our container class Account.  Fill each acciount with
+                //data from the rows, then dump them in a list.
+                
+                for (int i = 0; i < sqlDt.Rows.Count; i++)
+                {
+                    tmpSurveys.Add(new Survey
+                    {
+                        sID = Convert.ToInt32(sqlDt.Rows[i]["sID"]),
+                        qID = Convert.ToInt32(sqlDt.Rows[i]["qID"]),
+                        privacy = Convert.ToInt32(sqlDt.Rows[i]["privacy"]),
+                        asking_eID = Convert.ToInt32(sqlDt.Rows[i]["asking_eID"]),
+                        date = sqlDt.Rows[i]["date"].ToString(),
+                    });
+                    
+                }
+                //convert the list of accounts to an array and return!
+                return tmpSurveys;
+            }
+            else
+            {
+                //if they're not logged in, return an empty array
+                return tmpSurveys;
+            }
+        }
+
+        [WebMethod(EnableSession = true)]
+        public List<Survey> GetSurvey(int sID)
+        {
+            List<Survey> tmpSurveys = new List<Survey>(); // kind of a temporary list (we return this)
+
+            //WE ONLY LET LOGGED IN USERS SEE THEIR SURVEY!
+            if (Session["eID"] != null)
+            {
+                DataTable sqlDt = new DataTable("surveys");
+
+                string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+                string sqlSelect = $"select * from survey where survey.sID = {sID}";
+
+                MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+                MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+                //gonna use this to fill a data table
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+                //filling the data table
+                sqlDa.Fill(sqlDt);
+
+                //loop through each row in the dataset, creating instances
+                //of our container class Account.  Fill each acciount with
+                //data from the rows, then dump them in a list.
+
+                for (int i = 0; i < sqlDt.Rows.Count; i++)
+                {
+                    tmpSurveys.Add(new Survey
+                    {
+                        sID = Convert.ToInt32(sqlDt.Rows[i]["sID"]),
+                        qID = Convert.ToInt32(sqlDt.Rows[i]["qID"]),
+                        privacy = Convert.ToInt32(sqlDt.Rows[i]["privacy"]),
+                        asking_eID = Convert.ToInt32(sqlDt.Rows[i]["asking_eID"]),
+                        date = sqlDt.Rows[i]["date"].ToString(),
+                    });
+
+                }
+                //convert the list of accounts to an array and return!
+                return tmpSurveys;
+            }
+            else
+            {
+                //if they're not logged in, return an empty array
+                return tmpSurveys;
+            }
+        }
+
+
 
 
 
